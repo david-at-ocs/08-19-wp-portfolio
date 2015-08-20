e804caf8ce719b6fe11637ddfab524e079001c7e
David Gardetti <dgardetti@gmail.com>
2015-08-19 23:45:33 -0500

    fixed layout problem

--- page.php
+++ page.php
@@ -15,15 +15,23 @@ get_header( 'other' ); ?>
         <!-- <img src="img/shotglasses.png" alt="Shot glasses, black and white"> -->
         <div class="overlay">
           <div class="post_img">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/eschelon_screen_shot.png">
+            	<?php while ( have_posts() ) : the_post(); ?>
+
+            		<?php get_template_part('template-parts/content'); ?>
+
+            	<?php endwhile; // End of the loop. ?>
+            
+            <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/eschelon_screen_shot.png"> -->
           </div>
-          <div class="post_links">
+          <!-- <div class="post_links">
             <h1>Web work</h1>
             <p>Eschelon</p>
             <p><a href="#">http://www.example.com</a></p>
             <p><a href="#">http://www.github.com</a></p>
-          </div>
-        <div style="clear: both"></div>
+          </div> -->
+          <div style="clear: both"></div>
         </div>
       </section>
-<?php get_footer(); ?>
+
+            <?php get_footer(); ?>
+
